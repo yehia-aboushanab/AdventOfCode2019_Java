1321d27741d2e5459d1df195f7527671c01f8040
yehia <yehia_moustafa@outlook.com>
2020-07-04 00:11:13 +0800

    removing unnecessary variables (solution not commented)

--- Day3_2019.java
+++ Day3_2019.java
@@ -46,7 +46,6 @@ public class Day3_2019 {
         directions_guide.put("D",D_guide);
 
         for (String seg : path) {
-            System.out.println(seg);
             String direction = seg.substring(0,1);
             int distance = Integer.parseInt(seg.substring(1));
             int[] guide = directions_guide.get(direction);
@@ -55,17 +54,13 @@ public class Day3_2019 {
                 curry += guide[1];
                 step += 1;
                 int[] tmp_point = {currx,curry};
-                boolean not_found = true;
                 for (int j =0;j<points_list.size();j++){
                     int[] point = points_list.get(j);
                     if (Arrays.equals(tmp_point,point)){
-                        not_found = false;
+                        points_list.add(tmp_point);
+                        steps_list.add(step);
                         break;
                     }
-                }
-                if (not_found){
-                    points_list.add(tmp_point);
-                    steps_list.add(step);
                 }
             }
         }
@@ -83,35 +78,28 @@ public class Day3_2019 {
         //close the buffer reader
         reader.close();
 
-        String[] wire1_path = line1.split(",");
-        calc_points_return wire1_calc = calc_points(wire1_path);
-        String[] wire2_path = line2.split(",");
-        calc_points_return wire2_calc = calc_points(wire2_path);
+        calc_points_return wire1_calc = calc_points(line1.split(","));
+        calc_points_return wire2_calc = calc_points(line2.split(","));
         List<int[]>wire1_points = wire1_calc.get_points();
         List<Integer>wire1_steps = wire1_calc.get_steps();
         List<int[]>wire2_points = wire2_calc.get_points();
         List<Integer>wire2_steps = wire2_calc.get_steps();
         List<int[][]> intersection_points = new ArrayList<int[][]>();
         List<Integer> Manhattan_distance = new ArrayList<Integer>();
-        print(String.valueOf(wire1_points.size()));
         for (int i =0;i<wire1_points.size();i++){
             int[] point_wire1 = wire1_points.get(i);
-            print(String.valueOf(i));
             for (int j =0;j<wire1_points.size();j++){
                 int[] point_wire2 = wire2_points.get(j);
                 if (Arrays.equals(point_wire1,point_wire2)){
                     int[][] tmp_intersection_points = {point_wire1,point_wire2};
-                
                     intersection_points.add(tmp_intersection_points);
                     Manhattan_distance.add(  Math.abs(point_wire1[0]) + Math.abs(point_wire1[1]));
                     break;
                 }
             }
         }
-
-
-        int Min_Manhattan_distance_index  = Manhattan_distance.indexOf (Collections.min(Manhattan_distance));
-        print("minimum distance is: "+String.valueOf(Manhattan_distance.get(Min_Manhattan_distance_index)));
+        print("minimum distance is: "+
+            String.valueOf(Manhattan_distance.get(Manhattan_distance.indexOf (Collections.min(Manhattan_distance)))));
         List<Integer> Step = new ArrayList<Integer>();
         for (int[][] point : intersection_points ){
             int wire1_step_index = wire1_points.indexOf(point[0]);
@@ -120,8 +108,7 @@ public class Day3_2019 {
             int wire2_step = wire2_steps.get(wire2_step_index);
             Step.add(wire1_step+wire2_step);
         }
-        int Min_step_distance_index  = Step.indexOf (Collections.min(Step));
-        print("minimum steps are: "+String.valueOf(Step.get(Min_step_distance_index)));
+        print("minimum steps are: "+String.valueOf(Step.get(Step.indexOf (Collections.min(Step)))));
         
     }
     
