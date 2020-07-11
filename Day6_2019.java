5004d0d9b918b12576dbef9cb6d3a6e26e75af9c
yehia <yehia_moustafa@outlook.com>
2020-07-12 01:38:18 +0800

    Adding comments for DAY6 solution

--- Day6_2019.java
+++ Day6_2019.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 public class Day6_2019 {
+    //helper class to easily store all the objects and there details 
+    //such as the name of the object and the direct orbit for the orbit
     private static class Orbits{
         private List<String> Object_Name;
         private List<Integer> Number_of_orbits;
@@ -43,10 +45,7 @@ public class Day6_2019 {
         public int get_object_index(String Name){
             return this.Object_Name.indexOf(Name);
         }
-        public Integer Get_Number_of_orbits(String Name){
-            int object_index = this.Object_Name.indexOf(Name);
-            return this.Number_of_orbits.get(object_index);
-        }
+
         public Integer Get_Total_number_of_orbits(){
             int Total_numer_of_orbits=0;
             for (int i =0; i<this.Number_of_orbits.size();i++){
@@ -59,6 +58,7 @@ public class Day6_2019 {
         }
 
         public void Add_object(String Name){
+            //during the add object all three list are incremented to make the index of the same object the same for all three lists
             this.Object_Name.add(Name);
             this.Number_of_orbits.add(0);
             this.Connected_objects.add("");
@@ -79,36 +79,44 @@ public class Day6_2019 {
 		try {
             //Open the input.tx file to get the mass of the modules
 			reader = new BufferedReader(new FileReader(
-					"input_Day6.txt"));
+                    "input_Day6.txt"));
+            //Extracting the input data from the text file
             String line = reader.readLine();
 			while (line != null) {
                 String[] Object_Array = line.split(Pattern.quote(")"));
+                //checking or creating each object in every line of the input
                 for (int i=0 ; i<Object_Array.length;i++){
                     if (!Orbits_collection.object_exist(Object_Array[i])){
                         Orbits_collection.Add_object(Object_Array[i]);
                     }
                 }
+                //adding the dependant orbit 
                 Orbits_collection.Add_connected_object(Object_Array[1], Object_Array[0]);
+                //getting next line
                 line = reader.readLine();
             }
             //Solution for Part 1
-            // for (int i = 0 ; i <Orbits_collection.get_lenght_of_Object_List();i++){
-            //     String Object_Name = Orbits_collection.get_object_Name(i);
-            //     int number_of_orbits=0;
-            //     while (Orbits_collection.Got_connected_object(Object_Name)){
-            //         number_of_orbits = number_of_orbits+1;
-            //         int connected_index = Orbits_collection.get_object_index(Orbits_collection.get_connected_object(Object_Name));
-            //         Object_Name = Orbits_collection.get_object_Name(connected_index);
-            //     }
-            //     Object_Name = Orbits_collection.get_object_Name(i);
-            //     Orbits_collection.Change_orbit_num(Object_Name, number_of_orbits);
+            for (int i = 0 ; i <Orbits_collection.get_lenght_of_Object_List();i++){
+                String Object_Name = Orbits_collection.get_object_Name(i);
+                int number_of_orbits=0;
+                while (Orbits_collection.Got_connected_object(Object_Name)){
+                    //getting the series of the orbits for each object
+                    //and adding the total number of orbit for each object
+                    number_of_orbits = number_of_orbits+1;
+                    int connected_index = Orbits_collection.get_object_index(Orbits_collection.get_connected_object(Object_Name));
+                    Object_Name = Orbits_collection.get_object_Name(connected_index);
+                }
+                Object_Name = Orbits_collection.get_object_Name(i);
+                //saving the total number of orbits for each object
+                Orbits_collection.Change_orbit_num(Object_Name, number_of_orbits);
 
-            // }
-            // int Total_number_of_orbits = Orbits_collection.Get_Total_number_of_orbits();
-            // print("Total number of orbits: "+String.valueOf(Total_number_of_orbits));
+            }
+            //calculating the total number of orbits for all the objects
+            int Total_number_of_orbits = Orbits_collection.Get_Total_number_of_orbits();
+            print("Total number of orbits: "+String.valueOf(Total_number_of_orbits));
 
             //Solution for Part 2
-
+            //getting the direct orbit for the object named "YOU"
             String You_connected_object = Orbits_collection.get_connected_object("YOU");
             int orbits_For_object_YOU = 0;
             int orbits_For_object_SAN = 0;
@@ -118,7 +126,9 @@ public class Day6_2019 {
                 String San_Connected_object = Orbits_collection.get_connected_object("SAN");
                 orbits_For_object_SAN=0;
                 while (San_Connected_object!=""){
+                    //looping over all the direct and indirect orbits for the object named "SAN"
                     orbits_For_object_SAN=orbits_For_object_SAN+1;
+                    //Getting the first common orbit between YOU and SAN then breaking from both loops
                     if (You_connected_object.equals(San_Connected_object)){
                         break outerloop;
                     }
@@ -126,6 +136,7 @@ public class Day6_2019 {
                 }
                 You_connected_object = Orbits_collection.get_connected_object(You_connected_object);
             }
+            //calculating the lease number of orbits then deducting 2 which are teh direct orbits of the object YOU and SAN
             int least_number_of_orbits = orbits_For_object_SAN+orbits_For_object_YOU -2;
             print("Least number of orbits that need to be moved: "+String.valueOf(least_number_of_orbits));
         } catch (IOException e) {
